35dfd45da1975a58b7732f7ab80bccdb53a6727f
Frans Skarman <frans.skarman@gmail.com>
2015-11-13 22:52:35 +0100

    Added input manager

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 import sys, pygame
+from InputManager import *
 
 pygame.init()
 
-size = width, height = 1280, 720
+size = width, height = 720, 720
 speed = [0.2, 0.2]
 black = 0, 0, 0
 
@@ -12,10 +13,16 @@ roundaboutImage = pygame.image.load("media/roundabout.png")
 #roundabout = pygame.image.load("ball.gif")
 roundaboutRect = roundaboutImage.get_rect()
 
+roundaboutImage = pygame.transform.scale(roundaboutImage, (720,720));
+
+inputManager = InputManager()
+
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
-
+        
+        inputManager.onEvent(event);
+        
     screen.fill(black)
     screen.blit(roundaboutImage, roundaboutRect)
     pygame.display.flip()
