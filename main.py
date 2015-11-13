3aca67bdc06b4c7b4f4c2da7ef76cd43fcc17d32
Frans Skarman <frans.skarman@gmail.com>
2015-11-13 22:27:05 +0100

    Added media folder

--- main.py
+++ main.py
@@ -2,25 +2,20 @@ import sys, pygame
 
 pygame.init()
 
-size = width, height = 320, 240
+size = width, height = 1280, 720
 speed = [0.2, 0.2]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-ball = pygame.image.load("ball.gif")
-ballrect = ball.get_rect()
+roundaboutImage = pygame.image.load("media/roundabout.png")
+#roundabout = pygame.image.load("ball.gif")
+roundaboutRect = roundaboutImage.get_rect()
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
-
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(roundaboutImage, roundaboutRect)
     pygame.display.flip()
