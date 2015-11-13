573142a7b9a6cff70724e2f32ac25aa70f53d89b
Frans Skarman <frans.skarman@gmail.com>
2015-11-14 00:44:27 +0100

    CAAAR

--- car.py
+++ car.py
@@ -1,10 +1,51 @@
 import sys, pygame
+from Vec2 import *
+import math
 
+CAR_SCALE = 0.3;
 
-class car:
+BOARD_SIZE = 720
+
+MOVE_SPEED = 3
+
+class Car:
     def __init__(self, ang, rad):
         self.ang = ang
-        selg.rad = rad
+        self.rad = rad
+
+        self.img = None
+
+        self.imgTemplate = pygame.image.load("media/car.png")
+        self.box = self.imgTemplate.get_rect()
+
+        self.pos = Vec2(0,0)
+
+        self.size = Vec2(self.imgTemplate.get_width() * CAR_SCALE, self.imgTemplate.get_height() * CAR_SCALE)
+
+        #changing size
+        self.imgTemplate = pygame.transform.scale(self.imgTemplate, (int(self.size.x), int(self.size.y)))
+
+    def update(self, inputManager):
+        if(inputManager.moveLeft):
+            self.rad += MOVE_SPEED
+        if(inputManager.moveRight):
+            self.rad -= MOVE_SPEED
+
+        self.img = self.imgTemplate;
+        self.ang += 0.02
+
+        #self.img = pygame.transform.position(img, pos.getTuple())
+        self.img = pygame.transform.rotate(self.img, -(self.ang / math.pi * 180 - 90))
+
+        #Calculate position
+        self.pos = Vec2(
+                BOARD_SIZE / 2 + math.cos(self.ang) * self.rad - self.img.get_width() / 2,
+                BOARD_SIZE / 2 + math.sin(self.ang) * self.rad - self.img.get_height() / 2
+                )
+
+    def draw(self, screen):
+        screen.blit(self.img, (self.pos.x, self.pos.y), self.box)
+        #screen.blit(self.img, (0, 0), self.box)
         
 
 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 import sys, pygame
 from InputManager import *
+from car import *
 
 pygame.init()
 
-size = width, height = 720, 720
+BOARD_SIZE = 720
+
+size = width, height = BOARD_SIZE, BOARD_SIZE
 speed = [0.2, 0.2]
 black = 0, 0, 0
 
@@ -13,16 +16,21 @@ roundaboutImage = pygame.image.load("media/roundabout.png")
 #roundabout = pygame.image.load("ball.gif")
 roundaboutRect = roundaboutImage.get_rect()
 
-roundaboutImage = pygame.transform.scale(roundaboutImage, (720,720));
+roundaboutImage = pygame.transform.scale(roundaboutImage, (BOARD_SIZE,BOARD_SIZE));
 
 inputManager = InputManager()
+
+car = Car(0, 200);
 
 while 1:
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
         
         inputManager.onEvent(event);
-        
+
+    car.update(inputManager);
+
     screen.fill(black)
     screen.blit(roundaboutImage, roundaboutRect)
+    car.draw(screen);
     pygame.display.flip()
